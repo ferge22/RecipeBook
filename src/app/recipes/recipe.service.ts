35db6d84322aa945885f75efc0e593a2ca22ddfc
ferge22 <rytisspitrys@gmail.com>
2019-03-16 12:49:51 +0200

    using service for cross component communi

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,14 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
   ];
 
-  getRecipes(){
+  getRecipes() {
     // slice for copy of array
     return this.recipes.slice();
   }
