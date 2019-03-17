f5bbb975d448726f3038cfe22a750d1e7294f0f6
ferge22 <rytisspitrys@gmail.com>
2019-03-17 19:06:10 +0200

    adding routes to app

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,41 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 
 export class RecipeService {
   // listens to new Event
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+    new Recipe('Ultra Burger',
+    'Super Flavory Burger!',
+    'https://migogodense.dk/wp-content/uploads/2018/11/Aburgernator-800x480.jpg',
+    [
+      new Ingredient('Buns', 1),
+      new Ingredient('Meat ', 2),
+      new Ingredient('Cheese ', 4)
+    ]),
+    new Recipe('Shrimp Mix',
+    'Delicious tasty Sea food!',
+    'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+    [
+      new Ingredient('Big Shrimp', 6),
+      new Ingredient('Cucumber ', 2)
+    ]),
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     // slice for copy of array
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,9 @@
 import { Ingredient } from '../shared/ingredient.model'
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
-
+  // getting informed that ingredients array changed emitinu Ingredientu array
+  ingreduentsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -13,6 +15,14 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingreduentsChanged.emit(this.ingredients.slice());
+  }
+
+  // from recipe service
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingreduentsChanged.emit(this.ingredients.slice());
+
   }
 
 }
