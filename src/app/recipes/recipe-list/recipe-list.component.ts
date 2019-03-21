93ca165b178039fcfde5db4e6ae6aebd47e81bc8
ferge22 <rytisspitrys@gmail.com>
2019-03-21 15:08:23 +0200

    submitting the recipe edit form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,6 +17,13 @@ export class RecipeListComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.recipeService.recipesChanged
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipes = recipes;
+        }
+      );
+
     this.recipes = this.recipeService.getRecipes();
   }
 
