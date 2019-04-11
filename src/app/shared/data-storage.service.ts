3298b37cc97d47788e3b8d70e26c3f2528a80ee3
ferge22 <rytisspitrys@gmail.com>
2019-04-11 15:11:19 +0300

    fixing http requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { AuthService } from '../auth/auth.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 
@@ -10,18 +11,16 @@ export class DataStorageService {
 
   storeRecipes() {
     const token = this.authService.getToken();
-
     return this.http.put('https://ng-recipe-book-7f9b1.firebaseio.com/data.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
     const token = this.authService.getToken();
-    // console.log(token);
-    return this.http.get('https://ng-recipe-book-7f9b1.firebaseio.com/data.json?auth=' + token)
+    this.http.get<Recipe[]>('https://ng-recipe-book-7f9b1.firebaseio.com/data.json?auth=' + token)
       .subscribe(
-        (response: any[]) => {
-          console.log(response);
-          this.recipeService.setRecipes(response);
+        (recipes: Recipe[]) => {
+          console.log(recipes);
+          this.recipeService.setRecipes(recipes);
         }
       );
   }
